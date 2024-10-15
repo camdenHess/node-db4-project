b0f992e2e7743a5ceeaeedf2589e1599f9091a23
Camden Hess <camden.hess12@gmail.com>
2024-10-15 16:58:10 -0600

    'start of seed working'

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,16 @@
 const sharedConfig = {
     client: 'sqlite3',
-    useNullAsDefault: true,
     migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+    //sqlite3
+    useNullAsDefault: true,
     pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) }
 }
 
 module.exports = {
     development: {
         ...sharedConfig,
-        connection: { filename: './data/cook_book.db3'},
-        seeds: { directiory: './data/seeds '}
+        connection: { filename: './data/cook_book.db3'}
     },
     testing: {
         ...sharedConfig,
